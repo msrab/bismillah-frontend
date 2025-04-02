a66668bca89802ef97378b3fce3573628e72d971
msrab <31705735+msrab@users.noreply.github.com>
2025-04-02 15:01:31 +0200

    Connexion MySQL via Sequelize

--- app.js
+++ app.js
@@ -15,3 +15,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
+
+
+const sequelize = require('./config/database');
+
+sequelize.authenticate()
+  .then(() => console.log('✅ Connexion à la base de données réussie !'))
+  .catch(err => console.error('❌ Erreur de connexion à la base de données :', err));
