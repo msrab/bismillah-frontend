a33a18ddffcb89fbaef08ac5fed126e70392103c
msrab <31705735+msrab@users.noreply.github.com>
2025-05-15 14:29:54 +0200

    feat: créer les modèles Sequelize User et Restaurant

---
+++ models/Restaurant.js
@@ -0,0 +1,21 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Restaurant extends Model {}
+  Restaurant.init(
+    {
+      id:        { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name:      { type: DataTypes.STRING, allowNull: false },
+      company_number:{ type: DataTypes.STRING, allowNull: false, unique: true },
+      address_number:{ type: DataTypes.STRING },
+      phone:     { type: DataTypes.STRING },
+      email:     { type: DataTypes.STRING, allowNull: false, unique: true, validate:{ isEmail:true } },
+      password:  { type: DataTypes.STRING, allowNull: false },
+      logo:      { type: DataTypes.STRING },
+      nb_followers:{ type: DataTypes.INTEGER, defaultValue: 0 },
+    },
+    { sequelize, modelName: 'Restaurant', tableName: 'restaurants', timestamps: true }
+  );
+  return Restaurant;
+};
---
+++ models/User.js
@@ -0,0 +1,21 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init(
+    {
+      id:        { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      login:     { type: DataTypes.STRING, allowNull: false, unique: true },
+      email:     { type: DataTypes.STRING, allowNull: false, unique: true, validate:{ isEmail:true } },
+      password:  { type: DataTypes.STRING, allowNull: false },
+      address_number: { type: DataTypes.STRING },
+      firstname: { type: DataTypes.STRING },
+      surname:   { type: DataTypes.STRING },
+      phone:     { type: DataTypes.STRING },
+      avatar:    { type: DataTypes.STRING },
+    },
+    { sequelize, modelName: 'User', tableName: 'users', timestamps: true }
+  );
+  return User;
+};
