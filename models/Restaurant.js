e3165e74ae351be73863ed21f0a23cd33c27334f
msrab <31705735+msrab@users.noreply.github.com>
2025-07-08 09:51:38 +0200

    Update Restaurant.js - add is_active

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -5,15 +5,16 @@ module.exports = (sequelize) => {
   class Restaurant extends Model {}
   Restaurant.init(
     {
-      id:        { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      name:      { type: DataTypes.STRING, allowNull: false },
-      company_number:{ type: DataTypes.STRING, allowNull: false, unique: true },
-      address_number:{ type: DataTypes.STRING, allowNull: false },
-      phone:     { type: DataTypes.STRING },
-      email:     { type: DataTypes.STRING, allowNull: false, unique: true, validate:{ isEmail:true } },
-      password:  { type: DataTypes.STRING, allowNull: false },
-      logo:      { type: DataTypes.STRING },
-      nb_followers:{ type: DataTypes.INTEGER, defaultValue: 0 },
+      id:             { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name:           { type: DataTypes.STRING, allowNull: false },
+      email:          { type: DataTypes.STRING, allowNull: false, unique: true, validate:{ isEmail:true } },
+      password:       { type: DataTypes.STRING, allowNull: false },
+      company_number: { type: DataTypes.STRING, allowNull: false, unique: true },
+      address_number: { type: DataTypes.STRING, allowNull: false },
+      phone:          { type: DataTypes.STRING },
+      logo:           { type: DataTypes.STRING },
+      nb_followers:   { type: DataTypes.INTEGER, defaultValue: 0 },
+      is_active:      { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true }
     },
     { sequelize, modelName: 'Restaurant', tableName: 'restaurants', timestamps: true }
   );
