aeae5a7d34bc361d876851eb1206ff8713d0f789
msrab <31705735+msrab@users.noreply.github.com>
2025-07-15 20:35:13 +0800

    Update errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,7 @@
 function errorHandler(err, req, res, next) {
-  // Log l’erreur côté serveur (optionnel)
   console.error(err);
-
-  // Format de réponse uniforme
   res.status(err.status || 500).json({
-    message: err.message || 'Erreur serveur.',
-    // Optionnel : code personnalisé ou détails (jamais de stack trace en prod)
+    error: err.message || 'Erreur serveur.'
   });
 }
 
