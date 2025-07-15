be1a121fd0c2ed2c8eca0dacb0edb0810e3d40d7
msrab <31705735+msrab@users.noreply.github.com>
2025-07-15 14:47:29 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
+  dev: {
     username: process.env.DB_USER     || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME     || 'bismillah_app',
