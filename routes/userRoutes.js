16bc152f4f365fa31c648ee3e5b9d8eab7aaa4e7
msrab <31705735+msrab@users.noreply.github.com>
2025-07-17 22:28:32 +0800

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router  = express.Router();
 
 const { verifyToken } = require('../middlewares/authMiddleware');
+const { signupUserValidation } = require('../middlewares/UserValidation');
 const userController = require('../controllers/UserController');
 
 // Récupère le profil du user connecté
-// Exige un token JWT dans Authorization: Bearer <token>
 router.get('/me', verifyToken, userController.getProfile);
 
 // Met à jour le profil du user connecté
-// Corps JSON autorisé : { login?, email?, address_number?, firstname?, surname?, phone?, avatar? }
-router.put('/me', verifyToken, userController.updateProfile);
+router.put('/me', verifyToken, signupUserValidation, userController.updateProfile);
 
 // Supprime le compte du user connecté
 router.delete('/me', verifyToken, userController.deleteProfile);
