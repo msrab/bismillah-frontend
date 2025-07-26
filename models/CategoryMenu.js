71a3afeefd7e021fa71365767b7b26d3df5744fe
msrab <31705735+msrab@users.noreply.github.com>
2025-07-26 16:56:55 +0700

    Create CategoryMenu.js

---
+++ models/CategoryMenu.js
@@ -0,0 +1,26 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class CategoryMenu extends Model {
+    static associate(models) {
+      CategoryMenu.hasMany(models.CategoryMenuDescription, { as: 'descriptions', foreignKey: 'categoryMenuId' });
+      // Ajoute d'autres associations ici si besoin
+    }
+  }
+  CategoryMenu.init(
+    {
+      icon: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: 'CategoryMenu',
+      tableName: 'category_menus',
+      timestamps: false
+    }
+  );
+  return CategoryMenu;
+};
