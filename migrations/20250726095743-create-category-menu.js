79c3461500aef2b6688f5dd7ea1f40e18d3ae7ec
msrab <31705735+msrab@users.noreply.github.com>
2025-07-31 16:14:45 +0800

    update category menu migrations

--- migrations/20250726095743-create-category-menu.js
+++ migrations/20250726095743-create-category-menu.js
@@ -3,20 +3,26 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('category_menus', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      icon: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isValidated: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('category_menus');
   }
 };
--- migrations/20250726095815-create-category-menu-description.js
+++ migrations/20250726095815-create-category-menu-description.js
@@ -3,20 +3,43 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('category_menu_descriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      categoryMenuId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'category_menus',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      languageId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'languages',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('category_menu_descriptions');
   }
 };
